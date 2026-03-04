4eff99b7f2490ef70094ec830e7925ac1fa64a73
Oleksandr Melnychenko <oleksandrmelnychenko@users.noreply.github.com>
2026-03-04 08:46:24 +0200

    Update Package.swift checksum for v1.0.0 XCFramework

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "EcliptixOPAQUEBinary",
             url: "https://github.com/oleksandrmelnychenko/ecliptix-opaque-rs/releases/download/v1.0.0/EcliptixOPAQUE.xcframework.zip",
-            checksum: "UPDATE_AFTER_RELEASE"
+            checksum: "c742e7035868312d16c86b19e2a37ed8fa5eb01ef7d79ac5df20b2d3f0da791e"
         ),
         .target(
             name: "EcliptixOPAQUE",
