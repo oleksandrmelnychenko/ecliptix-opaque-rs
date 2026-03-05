b135baea8e36d0cd90e2b6e0383f63fedfe05242
Oleksandr Melnychenko <oleksandr.melnychenko@live.com>
2026-03-05 12:18:01 +0200

    Include binary target in product for C module access

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "EcliptixOPAQUE",
-            targets: ["EcliptixOPAQUESwift"]
+            targets: ["EcliptixOPAQUESwift", "EcliptixOPAQUE"]
         )
     ],
     targets: [
